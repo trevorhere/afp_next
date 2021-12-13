238f956fbeffb480c5e226da183961e6d959ed12
Trevor <trevoranthonylane@gmail.com>
2021-12-12 23:32:42 -0700

    f this thing

--- components/LogoCloud.js
+++ components/LogoCloud.js
@@ -2,18 +2,20 @@ import Image from 'next/image'
 
 export default function LogoCloud({logos}){
   return (
-    <div className="bg-gray-800 ">
-    <div className="max-w-7xl mx-auto py-16 px-4 sm:py-24 sm:px-6 lg:px-8">
-      <div className="lg:grid lg:grid-cols-2 lg:gap-8 flex justify-center items-center">
-        <h2 className="max-w-md mx-auto text-5xl font-medium text-white text-center lg:max-w-md lg:text-left">
-          Trusted by firefighting professionals everywhere.
-        </h2>
-        <div className="flow-root self-center mt-8 lg:mt-0">
-          <div className="-mt-4 -ml-8 flex flex-wrap justify-between lg:-ml-4">
+<>
+<div className="bg-gray-800 items-center flex items-center justify-center container mx-auto sm:px-6 lg:px-8">{/* Content goes here */}
+      <div className='flex flex-column flex-wrap space-between  my-14'>
+        <div className=' sm:basis-1/2 flex items-center'>
+          <h2 className="max-w-lg mx-auto text-5xl font-medium text-white text-center  lg:text-left">
+            Trusted by firefighting professionals everywhere.
+          </h2>
+        </div>
+        <div className=" sm:basis-1/2 mt-8 lg:mt-0 ">
+          <div className="flex flex-wrap justify-between w-full ">
             {logos.map(logo => (
-              <div key={logo.src} className="mx-8 min-w-42 flex flex-grow flex-shrink-0  justify-center align-center items-center lg:flex-grow-0 lg:m-4">
+              <div key={logo.src} className="m-4 w-24 h-auto lg:w-auto lg:h-auto flex flex-grow  justify-center align-center items-center ">
                 <img
-                  className='h-32'
+                  className='h-20 lg:h-24'
                   src={logo.src}
                   alt={logo.alt} 
                 />
@@ -22,7 +24,9 @@ export default function LogoCloud({logos}){
           </div>
         </div>
       </div>
-    </div>
-  </div>
+
+</div>
+</> 
+
   )
 }
