e116338b017f56d47c342d538c9f86ff443c128b
Trevor <trevoranthonylane@gmail.com>
2023-01-08 20:16:31 -0700

    remove layout field on logocloud section

--- components/Hero.js
+++ components/Hero.js
@@ -6,7 +6,7 @@ export default function Hero(){
       <header>
       </header>
       <main className="">
-        <section layout={'fill'} className="flex w-full h-screen items-center justify-center bg-[url('/images/Header_1.png')] bg-cover">
+        <section className="flex w-full h-screen items-center justify-center bg-[url('/images/Header_1.png')] bg-cover">
             <div className=' self-center text-center justify-center items-center text-white '>
               <h1 className="font-serif text-5xl">Anchor Flank <br/>& Pinch</h1>
               <h1 className="pt-4 italic text-xl font-serif">Leaders in aerial wildland firefighting</h1>
